ce5aa3998dfee43c6d6b9055685692167d6b9a6b
Minh <vanminh020698@gmail.com>
2019-12-22 09:25:00 +0700

    add script

---
+++ env.sh
@@ -0,0 +1,7 @@
+echo "1. install java"
+echo "2. install android studio"
+echo "3. create env"
+echo 'export ANDROID_HOME=/Users/$USER/Library/Android/sdk' >> ~/.bash_profile
+echo 'export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/build-tools/29.0.2' >> ~/.bash_profile
+source ~/.bash_profile
+# # # #
